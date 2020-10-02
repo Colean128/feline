d635fa4609c06d511373574b910e64b7ceb89a60
Zayne64 <11258171+Zayne64@users.noreply.github.com>
2020-10-02 22:30:43 +0100

    complet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    let file: Vec<String> = env::args().collect();
-    println!("{:?}", file);
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() == 1 {
+        println!("Feline by Colean128. Input a file location.")
+    } else {
+        let file = &args[1];
+
+        let mut string = fs::read_to_string(file)
+            .expect("The file could not be read.");
+
+        let len = string.len();
+        string.truncate(len - 1);
+        println!("{}", string);
+    }
 }
